c8eebb6ccd2833bd5f6cc6289d952015334b0ba6
Apple <lichunfeigood@gmail.com>
2023-06-27 15:01:46 +0800

    Initial Commit

---
+++ LoginPage/LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by Apple on 2023/6/27.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
